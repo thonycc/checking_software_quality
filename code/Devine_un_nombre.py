4e71ba4c0c78b9cd14c00b6b0e295e73dec63620
oliverice <37930980+oliverice@users.noreply.github.com>
2018-04-07 01:44:57 +0200

    Update Devine_un_nombre.py

--- code/Devine_un_nombre.py
+++ code/Devine_un_nombre.py
@@ -1,3 +1,7 @@
+#This program interacts with a user by asking to guess a number between 0 and 1001. The goal of the game
+# is to find the good number with the least attempts as possible.
+#This program is useful for testing abstract syntax tree methods because it has many loops.
+
 from random import randrange
 print("Ce jeu consiste à deviner un nombre compris entre 0 et 1000.\nPour abandonner taper 1001")
 
