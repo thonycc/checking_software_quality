79d95ab455e18f9da4ee09479fa5cf3803823218
oliverice <37930980+oliverice@users.noreply.github.com>
2018-04-25 15:37:54 +0200

    FSM complete and partial

---
+++ code/fsm_robot_maid_partial.py
@@ -0,0 +1,46 @@
+from fysom import *
+
+##========================================================================
+## Implementation of a robot maid who can cook, vaccum, clean the dishes and dance
+
+## Implementation of the finite state machine thanks to the Python module fysom
+fsm=Fysom(initial = 'sleeping',
+          events= [('sleep','cooking','sleeping'), ## Here are the transitions
+                   ('sleep','vacuuming','sleeping'),
+                   ('sleep','dancing','sleeping'),
+                   ('sleep','cleaning','sleeping'),
+                   ('cook','sleeping','cooking'),
+                   ('clean','cooking','cleaning'),
+                   ('dance','cleaning','dancing'),
+                   ('dance','sleeping','dancing'),
+                   ('dance','vacuuming','dancing'),
+                   ('vacuum','sleeping','vacuum'),
+                   ('vacuum','cleaning','vacuum'),
+                   ('vacuum','cooking','vacuum'),
+                   ])
+
+# While loop aiming at simulating interactions between the owner of the robot
+# and the robot.
+while(1):
+    i=input("what do you want from me ? ")
+    if i == 'sleep':
+        if fsm.current == 'sleeping':
+            print('I am already sleeping')
+        else :
+            fsm.sleep()
+            print('Ok, I am going to sleep')
+            break
+    elif i == 'vacuum':
+        fsm.vacuum()
+        print('Ok, I am going to vacuum')
+    elif i == 'cook' :
+        fsm.cook()
+        print('Ok, I am going to cook')
+    elif i == 'dance' :
+        fsm.dance()
+        print('Ok, I am going to dance')
+    elif i == 'clean' :
+        fsm.clean()
+        print('Ok, I am going to clean')
+    else :
+        print('Sorry, it is not possible')
